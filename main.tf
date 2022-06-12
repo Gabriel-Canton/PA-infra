53997479acb356294a3e5b9aea2cd819ffe7f884
Gabriel-Canton <gabrielcanton99@gmail.com>
2022-06-12 18:22:45 -0300

    Configurar backend y provider

--- main.tf
+++ main.tf
@@ -5,5 +5,18 @@ terraform {
     key = "terraform.tfstate"
     access_key = "snVh+5xM4ZJ1Qmh6hrRlZo3t9oTJSPvIE0h8PHfUqwSGttZaW6rOJv4ghbA59WTHWzCoM671ncmD+AStw7ghQw=="
   }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+  client_id = "e3b2c5e3-975d-4b5b-bb93-25f8b36c8ec7"
+  subscription_id = "a06ea5c9-35d5-42a7-b5cf-9d69fbfc20c9"
+  tenant_id = "c912770d-be52-40ca-9d40-77536a8b2f67"
+  client_secret = var.svc_acct_key
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "svc_acct_key" {
+  description = "Secret para la cuenta de servicio para Terraform"
+  sensitive = true
+}
