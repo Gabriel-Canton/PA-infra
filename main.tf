f2eaa96c7a9b95919089974f1771f2f290dfbf57
Gabriel-Canton <gabrielcanton99@gmail.com>
2022-06-19 15:09:52 -0300

    Use unique names for App Service resources

--- main.tf
+++ main.tf
@@ -15,7 +15,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   client_id       = "e3b2c5e3-975d-4b5b-bb93-25f8b36c8ec7"
   subscription_id = "a06ea5c9-35d5-42a7-b5cf-9d69fbfc20c9"
   tenant_id       = "c912770d-be52-40ca-9d40-77536a8b2f67"
@@ -31,17 +35,18 @@ locals {
 */
 module "HA_resources" {
   source = "./Modules/HA resources"
+  # Los nombres de los App Service tienen que ser globalmente únicos
   RESOURCES = [
     {
       app_insights_name     = "app-insights-1"
-      app_service_name      = "app-service-1"
+      app_service_name      = "app-svc-lkjsg-1"
       app_service_plan_name = "app-service-plan-1"
       location              = var.main_region
       resource_group_name   = var.RESOURCE_GROUP_NAMES[0]
     },
     {
       app_insights_name     = "app-insights-2"
-      app_service_name      = "app-service-2"
+      app_service_name      = "app-svc-lkajsw-2"
       app_service_plan_name = "app-service-plan-2"
       location              = var.secondary_region
       resource_group_name   = var.RESOURCE_GROUP_NAMES[1]
